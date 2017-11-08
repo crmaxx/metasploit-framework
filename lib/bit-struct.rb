adf6d0857ab8ca65441d679fe3f2cc9324765455
Maxim Zhukov <crmaxx@gmail.com>
2017-11-08 19:29:16 +0300

    Fix Fixnum deprecate warning

--- lib/bit-struct.rb
+++ lib/bit-struct.rb
@@ -2,10 +2,12 @@
 # A Convenience to load all field classes and yaml handling.
 # XXX: Pretty certian this monkeypatch isn't required in Metasploit.
 
-if "a"[0].kind_of? Fixnum
-  unless Fixnum.methods.include? :ord
-    class Fixnum
-      def ord; self; end
+if "a"[0].is_a?(Integer)
+  unless Integer.methods.include? :ord
+    class Integer
+      def ord
+        self
+      end
     end
   end
 end
